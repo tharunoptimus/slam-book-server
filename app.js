8ae223242c43455b5f4f3b54d24454b58095665d
tharunoptimus <tharunoptimus@outlook.com>
2023-07-05 23:44:51 +0530

    Added requireLogin middleware and registered api routes

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const express = require("express")
+require('express-async-errors')
 const app = express()
 require("./database")
-const { cors } = require("./middleware")
+const { cors, requireLogin } = require("./middleware")
 const port = process.env.PORT || 4003
 
 app.listen(port, () => {
@@ -16,8 +17,12 @@ app.use(express.urlencoded({ extended: true }))
 
 // API Routes
 const registerAPI = require("./routes/api/register")
+const loginAPI = require("./routes/api/login")
+const logoutAPI = require("./routes/api/logout")
 
 app.use("/api/register", cors, registerAPI)
+app.use("/api/login", cors, loginAPI)
+app.use("/api/logout", cors, logoutAPI)
 
 app.get("/", (_, res) => {
 	res.status(200).send("SLAM BOOK API ONLINE")
