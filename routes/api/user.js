df5e29e99457e0c276309a0a8b004304a00ba3f3
tharunoptimus <tharunoptimus@outlook.com>
2023-07-06 00:05:31 +0530

    Added user api endpoint to update details

--- routes/api/user.js
+++ routes/api/user.js
@@ -132,4 +132,71 @@ router.post("/:username", async (req, res) => {
 	}
 })
 
+router.put("/:username", async (req, res) => {
+	let { username } = req.params
+
+	let stuffThatCanBeUpdated = [
+		"firstName",
+		"lastName",
+		"instagram",
+		"twitter",
+		"facebook",
+		"linkedIn",
+		"website",
+		"about",
+		"emoji",
+	]
+
+	let userNameToBeUpdated = req.body.username
+
+	if (username !== userNameToBeUpdated)
+		return res
+			.status(403)
+			.send("You are not allowed to change your username")
+
+	let stuffToUpdate = {}
+
+	for (let key in req.body) {
+		if (stuffThatCanBeUpdated.includes(key)) {
+			stuffToUpdate[key] = req.body[key]
+		}
+	}
+
+	try {
+		let updatedUser = await User.findOneAndUpdate(
+			{ username: username },
+			{ $set: stuffToUpdate },
+			{ new: true }
+		)
+			.populate({
+				path: [
+					"_id",
+					"registerNumber",
+					"firstName",
+					"lastName",
+					"instagram",
+					"twitter",
+					"facebook",
+					"linkedIn",
+					"website",
+					"about",
+					"emoji",
+					"email",
+					"profilePic",
+				],
+			})
+			.catch((e) => {
+				console.log(e)
+				return res.status(500).send("Internal server error")
+			})
+
+		if (!updatedUser) return res.status(404).send("User not found")
+
+		return res.status(200).json({ user: updatedUser })
+	} catch (e) {
+		console.log(e)
+		return res.status(500).send("Internal server error")
+	}
+})
+
 module.exports = router
