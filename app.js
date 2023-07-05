4447200f281c4775254f1a2a858f7ac8769f67c3
tharunoptimus <tharunoptimus@outlook.com>
2023-07-06 00:24:35 +0530

    Added requireLogin for both userapi and adjective api

--- app.js
+++ app.js
@@ -17,11 +17,14 @@ app.use(express.urlencoded({ extended: true }))
 
 // API Routes
 const registerAPI = require("./routes/api/register")
+const userAPI = require("./routes/api/user")
+const adjectiveAPI = require("./routes/api/adjective")
 const loginAPI = require("./routes/api/login")
 const logoutAPI = require("./routes/api/logout")
 
 app.use("/api/register", cors, registerAPI)
 app.use("/api/user", cors, requireLogin, userAPI)
+app.use("/api/adjective", cors, requireLogin, adjectiveAPI)
 app.use("/api/login", cors, loginAPI)
 app.use("/api/logout", cors, logoutAPI)
 
