363956700068a41d5405aa134430e20a913f23fb
tharunoptimus <tharunoptimus@outlook.com>
2023-07-05 23:45:25 +0530

    Added user api route for search

--- routes/api/user.js
+++ routes/api/user.js
@@ -10,4 +10,40 @@ router.get("/", (_, res) => {
 	res.status(200).render("USER API ONLINE")
 })
 
+router.get("/:searchTerm", async (req, res) => {
+	let { searchTerm } = req.params
+
+	try {
+		let users = await User.find({
+			$or: [
+				{ firstName: { $regex: searchTerm, $options: "i" } },
+				{ lastName: { $regex: searchTerm, $options: "i" } },
+				{ username: { $regex: searchTerm, $options: "i" } },
+				{ email: { $regex: searchTerm, $options: "i" } },
+				{ registerNumber: { $regex: searchTerm, $options: "i" } },
+			],
+		})
+			.populate({
+				path: [
+					"_id",
+					"registerNumber",
+					"firstName",
+					"lastName",
+					"username",
+					"email",
+					"profilePic",
+				],
+			})
+			.catch((e) => {
+				console.log(e)
+				return res.status(500).send("Internal server error")
+			})
+
+		return res.status(200).json({ users })
+	} catch (e) {
+		console.log(e)
+		return res.status(500).send("Internal server error")
+	}
+})
+
 module.exports = router
