04cc5906a81a3acc4ae02f99533b95fa19436930
tharunoptimus <tharunoptimus@outlook.com>
2023-07-05 22:39:48 +0530

    Added field `updateSecret`

--- schemas/RegistrationSchema.js
+++ schemas/RegistrationSchema.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 const RegistrationSchema = new Schema({
 	registerNumber: { type: String, required: true, trim: true, unique: true },
 	sessionSecret: { type: String, required: true, trim: true },
+	updateSecret: { type: String, trim: true },
 },{ timestamps: true })
 
 let Registration = mongoose.model("Registration", RegistrationSchema)
